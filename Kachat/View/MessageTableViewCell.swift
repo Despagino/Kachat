04e1914a22cc3e118712ca76ed34ce814efb3584
Gino Tasis <gtasis84@gmail.com>
2022-03-09 17:16:42 -0500

    Added a xib file for each cell and added some styling to make the texts look nice

---
+++ Kachat/View/MessageTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MessageTableViewCell.swift
+//  Kachat
+//
+//  Created by Gino Tasis on 3/9/22.
+//
+
+import UIKit
+
+class MessageTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
