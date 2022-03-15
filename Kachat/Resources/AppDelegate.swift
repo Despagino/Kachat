09e135e12a2bb65e3932dcc609c15ec4b065f3dc
Gino Tasis <gtasis84@gmail.com>
2022-03-15 12:58:50 -0400

    Added IQKeyboardManager cocoapods to have a better keyboard layout

--- Kachat/Resources/AppDelegate.swift
+++ Kachat/Resources/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Firebase
+import IQKeyboardManagerSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         FirebaseApp.configure()
         let db = Firestore.firestore()
         
+        
+        IQKeyboardManager.shared.enable = true
         return true
     }
 
