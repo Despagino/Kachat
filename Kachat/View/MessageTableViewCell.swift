d570e3578e9d035d432658e9495ad08301c3c129
Gino Tasis <gtasis84@gmail.com>
2022-03-15 16:43:01 -0400

    Added left image view indicating the recipent were talking to

--- Kachat/View/MessageTableViewCell.swift
+++ Kachat/View/MessageTableViewCell.swift
@@ -13,8 +13,8 @@ class MessageTableViewCell: UITableViewCell {
     @IBOutlet weak var messageBubble: UIView!
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var avatarImage: UIImageView!
+    @IBOutlet weak var leftImageView: UIImageView!
     
-    var messages = MessageData()
 
     override func awakeFromNib() {
         super.awakeFromNib()
